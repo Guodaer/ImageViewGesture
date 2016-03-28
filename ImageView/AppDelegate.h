9b7f21d11ffe53f6f8451b5fe47e0b64d06ab4fe
GD1207 <1023251767@qq.com>
2016-03-28 14:23:42 +0800

    Initial Commit

---
+++ ImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageView
+//
+//  Created by xiaoyu on 16/3/28.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageView
+//
+//  Created by xiaoyu on 16/3/28.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
